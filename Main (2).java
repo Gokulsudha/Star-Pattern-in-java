81ac0ee70e82c575792cffeb8e4a76366f192dc7
Gokul B <139414683+Gokulsudha@users.noreply.github.com>
2025-01-05 15:03:04 +0530

    STAR PATTERN

---
+++ Main (2).java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        for(int row = 1;row <= 6;row++){
+            for(int col = 1;col<= row;col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
